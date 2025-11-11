58c08fdf48e74e80bc2475b523eddbaf492bfbd0
Aurora Scharff <aurora.sofie@gmail.com>
2025-11-11 09:28:47 +0100

    remove staletimes from starter

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     inlineCss: true,
-    staleTimes: {
-      dynamic: 30,
-    },
   },
   typedRoutes: true,
 };
